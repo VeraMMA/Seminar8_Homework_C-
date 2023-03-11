fa1d6d19a12e970bb07e529deb3c204438ecda92
Vera Maria <mymadonna89@mail.ru>
2023-03-11 20:18:58 +0600

    Homework 54 decided

--- Homework_54/Program.cs
+++ Homework_54/Program.cs
@@ -1,15 +1,4 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
-// каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
-
-static void Print(string word)
+﻿static void Print(string word)
 {
     System.Console.WriteLine(word);
 }
@@ -23,21 +12,19 @@ void FillMatrix(int[,] matr)
     {
         for (int columns = 0; columns < matr.GetLength(1); columns++)
         {
-
             matr[rows, columns] = rand.Next(1, 10);
-
         }
     }
 }
 
 void PrintMatrix(int[,] matr)
 {
+    Print("Печать массива.");
     for (int rows = 0; rows < matr.GetLength(0); rows++)
     {
         for (int columns = 0; columns < matr.GetLength(1); columns++)
         {
             Console.Write($"{matr[rows, columns]}\t");
-
         }
         System.Console.WriteLine();
     }
@@ -54,23 +41,36 @@ void SortToMin(int[,] matr)
             {
                 if (matr[rows, columns] > matr[rows, k])
                 {
-                    temp = matr [rows, columns];
+                    temp = matr[rows, columns];
                     matr[rows, columns] = matr[rows, k];
-                    matr [rows, k] = temp;
+                    matr[rows, k] = temp;
                 }
-
             }
-
-
         }
         System.Console.WriteLine();
     }
 }
 
+void PrintMatrixToMin(int[,] matr)
+{
+    Print(" Печать в порядке убывания элементов каждой строки массива.");
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int k = 0; k < matr.GetLength(1); k++)
+        {
+            Console.Write($"{matr[rows, k]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
 
-int[,] matrix = new int[3, 4];
+int m = 3;
+int n = 4;
+
+int[,] matrix = new int[m, n];
 
 FillMatrix(matrix);
 PrintMatrix(matrix);
 SortToMin(matrix);
+PrintMatrixToMin(matrix);
 
