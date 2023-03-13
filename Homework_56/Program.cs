ce606c409ad102397bf697ea2c1d82629390ee0d
Vera Maria <mymadonna89@mail.ru>
2023-03-13 20:49:45 +0600

    Homework 56 solved

--- Homework_56/Program.cs
+++ Homework_56/Program.cs
@@ -1,21 +1,20 @@
-﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить 
-// строку с наименьшей суммой элементов.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 5 2 6 7
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
-// наименьшей суммой элементов: 0 строка
+﻿Print("Найдите строку c наименьшей суммой элементов.");
+
+int m = 4;
+int n = 4;   
+int[,] matrix = new int[m, n];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+ 
 static void Print(string word)
 {
     System.Console.WriteLine(word);
 }
-Print("Найдите строку c наименьшей суммой элементов.");
 
 void FillMatrix(int[,] matr)
 {
-    Print("Заполните массив целыми числами");
+    Print("Заполнение массива целыми числами");
     Random rand = new Random();
     for (int rows = 0; rows < matr.GetLength(0); rows++)
     {
@@ -39,41 +38,34 @@ void PrintMatrix(int[,] matr)
     }
 }
 
-void SummRow(int[,] matr)
+int SummRow(int[,] matr, int rows)
 {
-    Print("Сумма элементов в каждой строке");
-    for (int rows = 0; rows < matr.GetLength(0); rows++)
-    {
-        bool uncount = false;
-        int summa = 0;
-        for (int columns = 0; columns < matr.GetLength(1); columns++)
-        {
-            summa = summa + matr[rows, columns];
-        }
-        int count = 1;
-        while (count < 2)
-        {
-            List<int> newArray = new List<int>() { };
-            newArray.Add(summa);
-            count++;
-            foreach (int item in newArray)
-            {
-                System.Console.WriteLine("Сумма строк:" + item);  
-            } 
-            int maxValue = newArray.Max();
-            int maxIndex = newArray.IndexOf(maxValue);
-            System.Console.WriteLine(maxValue);
-            System.Console.WriteLine(maxIndex);
-        }   uncount = true;
-    }       
-}          
+  int sumLine = matr[rows, 0];
+  for (int columns = 1; columns < matr.GetLength(1); columns++)
+  {
+    sumLine += matr[rows,columns];
+  }
+  return sumLine;
+}
+
+int minSumRow = 0;
+int sumLine = SummRow(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+  int temp = SummRow(matrix, i);
+  if (sumLine > temp)
+  {
+    sumLine = temp;
+    minSumRow = i;
+  }
+}
+Console.WriteLine($"{minSumRow} - cтpoкa c наименьшей суммой элементов равной: {sumLine}");
 
 
-int[,] matrix = new int[4, 4];
-int[] newArr = new int[4];
-FillMatrix(matrix);
-PrintMatrix(matrix);
-SummRow(matrix);
 
- 
-      
+
+
+
+
+
+
